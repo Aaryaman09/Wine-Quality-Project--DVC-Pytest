2ffc0c69f2e5a94f35adc4c9662ffaa87481d106
Aaryaman Sharma <31642776+Aaryaman09@users.noreply.github.com>
2023-09-09 17:00:35 +0530

    setup and tox done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='src',
+      version='0.0.1',
+      description='wine quality package',
+      author='aaryaman09',
+      packages=find_packages(),
+      license='MIT'
+    )
