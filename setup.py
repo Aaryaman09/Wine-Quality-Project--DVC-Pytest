b31f8c2b0670f935cb5326fb9684a1bc65cea08f
Aaryaman Sharma <31642776+Aaryaman09@users.noreply.github.com>
2023-09-10 23:25:42 +0530

    Starting Prediction service, created prediction folder

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='src',
-      version='0.0.1',
+      version='0.0.3',
       description='wine quality package',
       author='aaryaman09',
       packages=find_packages(),
