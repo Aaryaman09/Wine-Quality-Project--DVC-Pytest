fb6af4111b2198379c613ceded32d1e9bf98b86e
Aaryaman Sharma <31642776+Aaryaman09@users.noreply.github.com>
2023-09-17 19:28:51 +0530

    added prediction service

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='src',
-      version='0.0.3',
+      version='0.0.5',
       description='wine quality package',
       author='aaryaman09',
       packages=find_packages(),
